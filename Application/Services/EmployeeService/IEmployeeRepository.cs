79bd63a4b825ac84264ee2552ca604c5debefa03
Koorosh Dadsetan <kooroshdadsetan@outlook.com>
2023-05-19 23:40:56 +0330

    1

---
+++ Application/Services/EmployeeService/IEmployeeRepository.cs
@@ -0,0 +1,10 @@
+﻿using Application.Model.Response;
+
+namespace Application.Services
+{
+    public interface IEmployeeRepository
+    {
+        Task<List<EmployeeResponse>> GetAll();
+        Task<EmployeeResponse> GetEmployee(int id);
+    }
+}
---
+++ Domin/Contex/TestDbContext.cs
@@ -0,0 +1,38 @@
+﻿using Domin.Entity;
+using Microsoft.EntityFrameworkCore;
+
+namespace Domin.Contex;
+
+public partial class TestDbContext : DbContext
+{
+    public TestDbContext()
+    {
+    }
+
+    public TestDbContext(DbContextOptions<TestDbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Employee> Employees { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        => optionsBuilder.UseSqlServer("name=ConnectionString");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Employee>(entity =>
+        {
+            entity.Property(e => e.Id).HasColumnName("id");
+            entity.Property(e => e.Address).HasMaxLength(100);
+            entity.Property(e => e.FullName).HasMaxLength(50);
+            entity.Property(e => e.Mobile)
+                .HasMaxLength(11)
+                .IsUnicode(false);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
