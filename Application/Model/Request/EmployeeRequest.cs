9b72ac83a656fe94ad5d3c55802c2fbc8bad945b
Koorosh Dadsetan <kooroshdadsetan@outlook.com>
2023-05-25 21:55:38 +0330

    AddSortEmployees_2

---
+++ Application/Model/Request/EmployeeRequest.cs
@@ -0,0 +1,15 @@
+﻿namespace Application.Model.Request;
+
+public class EmployeeRequest
+{
+    public string FullName { get; set; } = null!;
+    public string? Mobile { get; set; }
+    public int? Age { get; set; }
+    public string? Address { get; set; }
+}
+
+public class EmployeeSort
+{
+    public string? Column { get; set; }
+    public bool? Desc { get; set; }
+}
